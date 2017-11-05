530a6baea94f7c6f37d092a97f32ed524873c7b7
Sabrina John <sabrina.john1990@gmail.com>
2017-11-05 16:07:44 -0500

    deleted old navbar javascript code

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
   $(document).ready(function() {
-      // if ($(window).width() < 1000) {
-      //   // $('#search-details').hide();
-      //   $('.search-service').on('click', function(e){
-      //     $('.search-details').slideToggle();
-      //     return false;
-      //   });
-      // }
-
-    // $('#nav .hamburger-menu i').on('click', function() {
-    //   $('.nav-bar').addClass('hidden');
-    //   $('.menu-items').toggleClass('hidden');
-    //   $('.container, #footer').addClass('hidden');
-    // });
-
-    // $('#nav ul li, .menu-items i').click(function (e) {
-    //   $('.menu-items').toggleClass('hidden');
-    //   $('.nav-bar').removeClass('hidden');
-    //   $('.container, #footer').removeClass('hidden');
-    // });
-
-    //testing new menu
     $(".menu-toggle").on('click', function(e) {
       e.preventDefault();
       $(this).toggleClass("on");
